a72a6608bc1755c8ea10ed127777562a10f55d6c
Guilherme <guilhermelegramante@gmail.com>
2022-06-22 16:05:34 -0300

    Initial setup

---
+++ resources/views/index.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Filament</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+
+    @livewireStyles
+</head>
+
+<body>
+
+    <livewire:counter />
+
+
+
+
+    @livewireScripts
+</body>
+
+</html>
---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,12 @@
+<div>
+    <div class="container mx-auto px-4 py-14 sm:px-6 xl:px-12">
+        <div class="flex flex-col items-center justify-center space-y-6 text-center">
+            <h1 class="text-4xl font-bold tracking-normal sm:text-5xl lg:text-6xl">{{ $count }}</h1>
+            <p class="max-w-screen-sm text-lg text-gray-600 sm:text-2xl">We write about tips, best tools, and resources
+                to create and sell interactive online programming courses.</p>
+            <button wire:click='increment' class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Button
+            </button>
+        </div>
+    </div>
+</div>
